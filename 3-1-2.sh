c34590c0c1ad0c6dd99591cab491ecf063e88183
CodexHere <codexhere@outlook.com>
2025-08-17 02:31:09 -0500

    Minor updates to startup and window order

--- 3-1-2.sh
+++ 3-1-2.sh
@@ -12,30 +12,35 @@ flatpak ps | grep qpwgraph
 ps ux | grep reaper | grep -v grep
 (( $? != 0 )) && reaper &
 
-# Open Music (Remote)
-REMOTE_EXEC "google-chrome 'https://www.youtube.com/watch?v=lIrJ7vTMNOc&list=ULcxqQ59vzyTk'" &
+# Delete Recently Used History, and MOTD semaphore file
+rm ~/.local/share/recently-used.xbel
+rm /tmp/.motd_shown_*
 
 # Open Chat Popout
 google-chrome --new-window 'https://www.twitch.tv/popout/CodexHere/chat?popout=' &
-sleep 1
+# Start MIDI Control Overlay (opens browser window)
+/media/NFG_GDrive_NFG/CodexHere/Web/midi-control/dockerStart.sh
 
 # Open Stream Management Stuffs
-google-chrome 'https://dashboard.twitch.tv/u/CodexHere/stream-manager' &
-sleep 1
-xdg-open 'https://play.pretzel.rocks/'
-
-# Start MIDI Control Overlay
-/media/NFG_GDrive_NFG/CodexHere/Web/midi-control/dockerStart.sh
+google-chrome --new-window 'https://dashboard.twitch.tv/u/CodexHere/stream-manager' &
+google-chrome 'https://www.youtube.com/feed/playlists' &
+google-chrome 'https://myactivity.google.com/myactivity?hl=en&utm_source=google-account' &
 
 # OBS!
 # Run `reaper` if it isn't already
 ps aux | grep -v grep | grep obs
 (( $? != 0 )) && obs &
+
 sleep 10 
+
 ssh nfgarmy.local docker restart obs-ws_toxicice
+
 sleep 5
+
 toggleWebcam 1
+
 switchToScene "Starting Soon"
 
 # Open VSCode to edit our Ticker Text
 code /media/NFG_GDrive_NFG/NFG\ Graphics/Streaming_Redux/Overlay/web/overlay-ticker/ticker.txt
+
