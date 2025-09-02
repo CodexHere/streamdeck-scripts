19cc3427d9e8ab693d0167561a925ef6dc304a28
CodexHere <codexhere@outlook.com>
2025-09-01 20:17:45 -0500

    Cleanup for start script to open history/cleanup stuff, changed order of things for optimized load.
    Opening ticker folder instead of file, so I can swap between other files for ease

--- 3-1-2.sh
+++ 3-1-2.sh
@@ -23,16 +23,20 @@ google-chrome --new-window 'https://www.twitch.tv/popout/CodexHere/chat?popout='
 
 # Open Stream Management Stuffs
 google-chrome --new-window 'https://dashboard.twitch.tv/u/CodexHere/stream-manager' &
+sleep 1 #time to let the new window load
 google-chrome 'https://www.youtube.com/feed/playlists' &
 google-chrome 'https://myactivity.google.com/myactivity?hl=en&utm_source=google-account' &
+google-chrome 'chrome://settings/clearBrowserData' &
 
-# OBS!
-# Run `reaper` if it isn't already
+# Wait for everything to open, particularly MIDI Control
+sleep 5
+
+# Run `OBS` if it isn't already
 ps aux | grep -v grep | grep obs
 (( $? != 0 )) && obs &
 
+# Wait for OBS to start, and then remotely restart obs-ws-http
 sleep 10 
-
 ssh nfgarmy.local docker restart obs-ws_toxicice
 
 sleep 5
@@ -42,5 +46,5 @@ toggleWebcam 1
 switchToScene "Starting Soon"
 
 # Open VSCode to edit our Ticker Text
-code /media/NFG_GDrive_NFG/NFG\ Graphics/Streaming_Redux/Overlay/web/overlay-ticker/ticker.txt
+xdg-open /media/NFG_GDrive_NFG/NFG\ Graphics/Streaming_Redux/Overlay/web/overlay-ticker
 
